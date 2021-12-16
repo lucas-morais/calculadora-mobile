c1b57e1ad9ad06b24d3bc7e170959d0acd174e98
Lucas Toledo <lucasmoraisss@gmail.com>
2021-12-16 14:05:55 -0300

    buttons operations

--- calculadora/App.js
+++ calculadora/App.js
@@ -4,28 +4,42 @@ import Display from './components/Display';
 import PadButton from './components/PadButton';
 
 export default function App() {
+
   const [ displayValue, setDisplayValue ] = useState('0');
+  
+  const addDigit = (n) => {
+    setDisplayValue(n)
+  }
+
+  const clearMemory = () => {
+    setDisplayValue('0');
+  }
+
+  const setOperation = (operation) => {
+
+  }
+
   return (
     <View style={styles.container}>
       <Display value={ displayValue } />
       <View style={ styles.buttons }>   
-        <PadButton label="AC"/>      
-        <PadButton label="/"/>      
-        <PadButton label="7"/>      
-        <PadButton label="8"/>      
-        <PadButton label="9"/>      
-        <PadButton label="*"/>      
-        <PadButton label="4"/>      
-        <PadButton label="5"/>      
-        <PadButton label="6"/>      
-        <PadButton label="-"/>      
-        <PadButton label="1"/>      
-        <PadButton label="2"/>      
-        <PadButton label="3"/>      
-        <PadButton label="+"/>      
-        <PadButton label="0"/>      
-        <PadButton label="."/>      
-        <PadButton label="="/>      
+        <PadButton label="AC" triple onClick={clearMemory}/>      
+        <PadButton label="/" operation onClick={() => { setOperation('/')}}/>      
+        <PadButton label="7" onClick={ () => { addDigit(7)}}/>      
+        <PadButton label="8" onClick={ () => { addDigit(8)}}/>      
+        <PadButton label="9" onClick={ () => { addDigit(9)}}/>      
+        <PadButton label="*" operation onClick={() => { setOperation('*')}}/>      
+        <PadButton label="4" onClick={ () => { addDigit(4)}}/>      
+        <PadButton label="5" onClick={ () => { addDigit(5)}}/>      
+        <PadButton label="6" onClick={ () => { addDigit(6)}}/>      
+        <PadButton label="-" operation onClick={() => { setOperation('-')}}/>      
+        <PadButton label="1" onClick={ () => { addDigit(1)}}/>      
+        <PadButton label="2" onClick={ () => { addDigit(2)}}/>      
+        <PadButton label="3" onClick={ () => { addDigit(3)}}/>      
+        <PadButton label="+" operation onClick={() => { setOperation('+')}}/>      
+        <PadButton label="0" double onClick={ () => { addDigit(0)}}/>      
+        <PadButton label="." onClick={ () => { addDigit('.')}}/>      
+        <PadButton label="=" operation onClick={() => { setOperation('=')}}/>      
       </View>
       <StatusBar style="auto" />
     </View>
--- calculadora/components/PadButton/index.js
+++ calculadora/components/PadButton/index.js
@@ -6,23 +6,39 @@ import {
   TouchableHighlight,
 } from 'react-native';
 
+const baseSide = Dimensions.get('window').width / 4;
+
 const styles = StyleSheet.create({
   button: {
     fontSize: 40,
-    height: Dimensions.get('window').width / 4,
-    width: Dimensions.get('window').width / 4,
+    height: baseSide,
+    width: baseSide,
     padding: 20,
     backgroundColor: '#f0f0f0',
     textAlign: 'center',
     borderWidth: 1,
     borderColor: '#888',
+  },
+  opperationButton: {
+    color:'#fff',
+    backgroundColor: '#f8a231',
+  },
+  buttonDouble: {
+    width: baseSide * 2,
+  },
+  buttonTripple: {
+    width: baseSide * 3,
   }
 });
 
-export default ({ label, onClick}) => {
+export default ({ label, onClick, double, triple, operation}) => {
+  const stylesButton = [styles.button]
+  if(double) stylesButton.push(styles.buttonDouble);
+  if(triple) stylesButton.push(styles.buttonTripple);
+  if(operation) stylesButton.push(styles.opperationButton);
   return (
     <TouchableHighlight onPress={ onClick}>
-      <Text style={ styles.button }>{ label }</Text>
+      <Text style={ stylesButton }>{ label }</Text>
     </TouchableHighlight>
   );
 }
